d3fcc0ec87aabf659f929946c7925276e2afdd22
Boyan A <80381466+BoyanAleksandrov@users.noreply.github.com>
2024-03-01 11:05:09 +0200

    Initial commit

---
+++ thesis-lawyer/thesis-lawyer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace thesis_lawyer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
