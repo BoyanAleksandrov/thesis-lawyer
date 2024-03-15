5531887c37b5f6af0f53e7075006a9093c83e074
Boyan A <80381466+BoyanAleksandrov@users.noreply.github.com>
2024-03-15 10:44:32 +0200

    Main chat functionality

--- thesis-lawyer/thesis-lawyer/Controllers/ChatController.cs
+++ thesis-lawyer/thesis-lawyer/Controllers/ChatController.cs
@@ -1,12 +1,83 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using thesis_lawyer.Models;
-namespace thesis_lawyer.Controllers;
+﻿using IBM.Watson.Assistant.v2;
 
-public class ChatController : Controller
+using Microsoft.AspNetCore.Mvc;
+using System;
+using IBM.Cloud.SDK.Core.Authentication.Iam;
+
+namespace thesis_lawyer.Controllers
 {
-    public IActionResult chatlawyer()
+    public class ChatController : Controller
     {
-        return View();
+        private string _apiKey = "KQpwSLvnAsVDzTnm_382btKs_B30IhafZyavsejojuCF";
+        private string _draftId = "7f5820e4-4b80-41a2-ba68-db3789ebe733";
+        private AssistantService _assistantService;
+        private string _sessionId;
+
+        public ChatController()
+        {
+            IamAuthenticator authenticator = new IamAuthenticator(apikey: _apiKey);
+            _assistantService = new AssistantService("2021-06-14", authenticator);
+            _assistantService.SetServiceUrl("https://api.eu-gb.assistant.watson.cloud.ibm.com/instances/7f39927a-d244-4bcc-b08f-1f5367946dcf");
+
+            // Create session when ChatController is instantiated
+            CreateSession();
+        }
+
+        private void CreateSession()
+        {
+            try
+            {
+                var result = _assistantService.CreateSession(assistantId: _draftId);
+                _sessionId = result.Result.SessionId;
+                Console.WriteLine("============SESSION CREATED============");
+                
+            }
+            catch (Exception ex)
+            {
+                // Handle exception
+                Console.WriteLine("Error creating session: " + ex.Message);
+            }
+        }
+
+        // Action method to display the chat view
+        public IActionResult chatlawyer()
+        {
+            return View();
+        }
+
+        // Action method to send a message to Watson Assistant
+        [HttpPost]
+        public IActionResult SendMessageToWatson(string message)
+        {
+            var response = SendMessage(message);
+            Console.WriteLine(message);
+            return Json(new { response });
+        }
+
+        // Add other controller actions as needed...
+
+        private string SendMessage(string message)
+        {
+            try
+            {
+                var result = _assistantService.Message(
+                    assistantId: _draftId,
+                    sessionId: _sessionId,
+                    input: new IBM.Watson.Assistant.v2.Model.MessageInput()
+                    {
+                        MessageType = "text",
+                        Text = message
+                    }
+                );
+                Console.WriteLine(message);
+
+                return result.Result.Output.Generic[0].Text;
+            }
+            catch (Exception ex)
+            {
+                // Handle exception
+                return "Error: " + ex.Message;
+            }
+        }
     }
 }
--- thesis-lawyer/thesis-lawyer/wwwroot/js/site.js
+++ thesis-lawyer/thesis-lawyer/wwwroot/js/site.js
@@ -1,4 +1,64 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var Message = function (arg) {
+    this.text = arg.text;
+    this.message_side = arg.message_side;
+    this.draw = function () {
+        var $message = $($('.message_template').clone().html());
+        $message.addClass(this.message_side).find('.text').html(this.text);
+        $('.messages').append($message);
+        setTimeout(function () {
+            $message.addClass('appeared');
+        }, 0);
+    };
+    return this;
+};
 
-// Write your JavaScript code.
+$(function () {
+    var getMessageText = function () {
+        var $message_input = $('.message_input');
+        return $message_input.val();
+    };
+
+    var sendMessage = function (text) {
+        var $messages, message;
+        if (text.trim() === '') {
+            return;
+        }
+        $('.message_input').val('');
+        $messages = $('.messages');
+        message = new Message({
+            text: text,
+            message_side: 'right'
+        });
+        message.draw();
+
+        // Make AJAX request to .NET MVC controller
+        $.ajax({
+            url: '/Chat/SendMessageToWatson/',
+            type: 'POST',
+            data: {
+                message: text
+            },
+            success: function (response) {
+                var answer = response.response;
+                var answerMessage = new Message({
+                    text: answer,
+                    message_side: 'left'
+                });
+                answerMessage.draw();
+                $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
+            }
+        });
+    };
+
+    $('.send_message').click(function () {
+        sendMessage(getMessageText());
+    });
+
+    $('.message_input').keyup(function (e) {
+        if (e.which === 13) {
+            sendMessage(getMessageText());
+        }
+    });
+
+   
+});
